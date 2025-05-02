1782a324fa894167f57d4eb74da3e75cffa55679
Manoj Rai <manojrai@192.168.1.238>
2025-05-02 20:38:24 +0545

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,5 +1,6 @@
 // Karl Pearson and Rank
 #include<iostream>
+#include<iomanip>
 #include<cmath>
 using namespace std;
 class dis_math{
@@ -74,23 +75,88 @@ class dis_math{
 
 
     }
+    void regression()
+    {
+        float a[n],deno,nume;
+        float res_a,res_b,mean_y,mean_x;
+        int ans;
+        cout<<"Choose either one of the regression coefficient"<<endl<<"1. X on Y"<<endl<<"2. Y on X"<<endl;
+        cin>>ans;
+        for(int i=0;i<n;i++){
+            if(i==0){
+            cout<<"(Total number of data) n = ";
+            cin>>a[i];
+            }
+            else if(i==1){
+                cout<<"(Sum of x)Ex = ";
+                cin>>a[i];
+            }
+            else if(i==2){
+                cout<<"(Sum of y) Ey = ";
+                cin>>a[i];
+            }
+            else if(i==3){
+                cout<<"(Sum of x and y) E(xy)= ";
+                cin>>a[i];
+            }
+            else if(i==4){
+            cout<<"(Sum of X squared) E(x^2) = ";
+            cin>>a[i];
+            }
+            else{
+            cout<<"(Sum of Y squared) E(y^2) = ";
+            cin>>a[i];
+            }
+        
+        }
+        nume = (a[0]*a[3])-(a[1]*a[2]);  // n*E(xy) - Ex*Ey
+        mean_x = a[1]/n;
+        mean_y = a[2]/n;
+        if(ans == 1){
+        deno =  (a[0]*a[5])-(a[2]*a[2]); 
+        cout<<"So for calculating X on Y we consider a linear equation in terms of X"<<endl<<"Which means that X is considered as dependent variable and Y is considered as independent variable"<<endl;
+        cout<<"So, the linear equation is:"<<endl<<"X = a + bY"<<endl<<"Where a and b are the parameters of regression coefficient"<<endl;
+        cout<<"So calculating a and b and giving the result or the final equation"<<endl;
+        res_b = nume/deno;
+        res_a = mean_x - (res_b*mean_y);
+        cout<<"Now, the final equation is:"<<endl<<"X = "<<res_a<<" + "<<res_b<<" Y "<<endl;
+    }
+    else {
+        deno = (a[0]*a[4])-(a[1]*a[1]);
+        cout<<"So for calculating Y on X we consider a linear equation in terms of Y"<<endl<<"Which means that Y is considered as dependent variable and X is considered as independent variable"<<endl;
+        cout<<"So, the linear equation is:"<<endl<<"Y = a + bX"<<endl<<"Where a and b are the parameters of regression coefficient"<<endl;
+        cout<<"So calculating a and b and giving the result or the final equation"<<endl;
+        res_b = nume/deno;
+        res_a = mean_y - (res_b*mean_x);
+        cout<<"Now, the final equation is:"<<endl<<"Y = "<<res_a<<" + "<<res_b<<" X "<<endl;
+    }
+    
+    }
 };
 int main()
 {
     dis_math s;
     int ans;
     cout<<"This is a program that can calculate the following\n";
-    cout<<"1.Karl Pearson Method\n"<<"2. Spearman's Rank Correlation\n";
+    cout << "========== MENU ==========" << endl;
+    cout << "1.Karl Pearson Method" << endl 
+         << "2. Spearman's Rank Correlation" << endl 
+         << "3. Regression Coefficient" << endl;
+    cout << "==========================" << endl;
     cin>>ans;
     switch(ans)
     {
         case 1: 
-        cout<<"Enter the details to be intepreted below (Karl Pearson Method)\n";
+        cout<<"Enter the details to be intepreted below (Karl Pearson Method)"<<endl;
         s.karl();
         break;
         case 2:
-        cout<<"Enter the details to be intepreted below (Spearman's Rank Correlation)\n";
+        cout<<"Enter the details to be interpreted below (Spearman's Rank Correlation)"<<endl;
         s.rank();
+        break;
+        case 3:
+        cout<<"Enter the details to be interpreted below (Regression Coefficient)"<<endl;
+        s.regression();
         break;
         default:
         cout<<"Invalid Number!! Try Again!!";
